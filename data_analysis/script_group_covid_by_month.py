a022f83bf8db6f455a19ad0ee47973b274dce0dc
Ana Lima <ayf.lima@gmail.com>
2022-11-11 20:58:10 -0300

    feat: Separate covid data by month

---
+++ data_analysis/script_group_covid_by_month.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import sys
+sys.path.append('./data_analysis')
+import os
+
+from modules.DataPreparation import DataPreparation
+from modules.DataGrouping import DataGrouping
+
+if 'data_analysis' in os.getcwd():
+    data_folder = '../data/'
+else:
+    data_folder = './data/'
+
+source_folder_path = data_folder + 'covid/treated_data/'
+destination_folder_path = data_folder + 'covid/treated_data/'
+
+covid_data = pd.read_csv(source_folder_path+ '/data.csv')
+
+dp = DataPreparation()
+
+covid_data = dp.transform_to_datetime(covid_data, ['date'])
+covid_data = dp.transform_to_time_series(covid_data, 'date', drop=True)
+
+dg = DataGrouping(covid_data)
+
+mean_grouped_trips_month, std_grouped_trips_month = dg.group_by_month(covid_data)
+
+mean_grouped_trips_month.to_csv(destination_folder_path + 'covid_grouped_by_month.csv')
+std_grouped_trips_month.to_csv(destination_folder_path + 'covid_grouped_by_month_std.csv')
