c837363caf0c11198e3c3000d55e12689555d223
Ana Lima <ayf.lima@gmail.com>
2022-11-27 20:47:50 -0300

    chore: Change COVID start date

--- data_analysis/script_separate_trips_covid.py
+++ data_analysis/script_separate_trips_covid.py
@@ -24,13 +24,13 @@ dp = DataPreparation()
 trips = dp.transform_to_datetime(trips, ['starttime', 'stoptime'])
 trips = dp.transform_to_time_series(trips, 'starttime')
 
-trips_before_covid = trips[:'2020-3-05']
+trips_before_covid = trips[:'2020-3-23']
 trips_before_covid.to_csv(destination_folder_path + 'trips_before_covid.csv')
 
 second = time.time()
 print('Before covid: Save to csv completed. Time = {time}'.format(time = second - first))
 
-trips_after_covid = trips['2020-3-06':]
+trips_after_covid = trips['2020-3-24':]
 trips_after_covid.to_csv(destination_folder_path + 'trips_after_covid.csv')
 
 third = time.time()
