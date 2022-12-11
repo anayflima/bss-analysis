410215823d0812ae9d370703ea8b51ccceeba57a
Ana Lima <ayf.lima@usp.br>
2022-12-10 22:08:03 -0300

    feat: Enhance the CorrelationWithCovid module's methods

--- data_analysis/modules/CorrelationWithCovid.py
+++ data_analysis/modules/CorrelationWithCovid.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import sys
 sys.path.append('./data_analysis')
 sys.path.append('../../data_analysis')
@@ -25,12 +26,18 @@ class CorrelationWithCovid:
         self.covid_data = covid_data
     
     def import_and_prepare_trips_data(self,
-            file_path = 'trips/preprocessed/grouped/trips_grouped_by_{frequency}_mean.csv'):
+            file_path = 'trips/preprocessed/grouped/trips_grouped_by_{frequency}_mean.csv', period = 'all'):
         file_path = file_path.format(frequency = self.frequency)
         trips = pd.read_csv(self.data_folder + file_path)  
         dp = DataPreparation()
         trips = dp.transform_to_datetime(trips, ['date'])
         trips = dp.transform_to_time_series(trips, 'date', drop = True)
+        trips = trips['2018-02':]
+        if period == 'before':
+            trips = trips[:os.environ['LAST_DAY_BEFORE_COVID']]
+        elif period == 'during':
+            # trips = trips[os.environ['FIRST_DAY_COVID']:]
+            trips = trips['2019-05':]
         self.trips = trips
     
     def merge_trips_and_covid_data(self):
@@ -96,6 +103,7 @@ class CorrelationWithCovid:
             ax.axvline('2021-03-15', color="red", linestyle="--",  label='Fase Emergencial')
             ax.axvline('2021-08-17', color="green", linestyle="--",  label='Fase Verde')
             ax.axvline('2021-11-01', color="m", linestyle="--",  label='Fim das restrições')
+            
         plt.ylim(bottom=0)
         plt.axis([None, None, 0, None])
         ax.legend(bbox_to_anchor=(1.15, 1), loc='upper left')
@@ -110,7 +118,7 @@ class CorrelationWithCovid:
         ax.axvspan(start, end, facecolor=color, alpha=0.1)
         ax.set_xlim(x_left, x_right)
 
-    def plot_variable_and_covid_together(self, variable, covid_variables, show_key_pandemic_moments = True, show_all_phases = False,save = False):
+    def plot_variable_and_covid_together(self, variable, covid_variables, show_key_pandemic_moments = True, show_all_phases = False, save = False):
         if not isinstance(covid_variables, list):
             covid_variables = [covid_variables]
         
@@ -121,17 +129,20 @@ class CorrelationWithCovid:
         fig.set_figwidth(20)
         fig.set_figheight(10)
 
-        ax.plot(trips_and_covid_plot.index, trips_and_covid_plot[variable], 'g-', label = variable)
+        # ax.plot(trips_and_covid_plot.index, trips_and_covid_plot[variable], 'g', marker = 'o', label = variable)
+        ax.plot(trips_and_covid_plot.index, trips_and_covid_plot[variable], 'g', label = variable)
         
         if show_key_pandemic_moments:
-            ax.axvline(pd.Timestamp('2020-03-24'), color="black", linestyle="--",  label='Lockdown in SP', linewidth = 3.0)
-            ax.axvline(pd.to_datetime('2021-11-01'), color="m", linestyle="--",  label='End of COVID-19 restrictions', linewidth = 3.0)
+            ax.axvline(pd.to_datetime('2020-03-24'), color="black", linestyle="--",  label='Lockdown in SP', linewidth = 3.0)
+            ax.axvline(pd.to_datetime('2021-11-01'), color="m", linestyle="--",  label='Fim das restrições', linewidth = 3.0)
+            # ax.axvline(pd.to_datetime('2018-02-01'), color="black", linestyle="--",  label='Início dos dados')
         if show_all_phases:
             ax.axvline(pd.to_datetime('2020-05-27'), color="purple", linestyle="--",  label='Plano SP de retomada consciente')
             ax.axvline(pd.to_datetime('2020-10-06'), color="green", linestyle="--",  label='Fase Verde')
             ax.axvline(pd.to_datetime('2020-11-30'), color="yellow", linestyle="--",  label='Fase Amarela')
             ax.axvline(pd.to_datetime('2021-03-15'), color="red", linestyle="--",  label='Fase Emergencial')
             ax.axvline(pd.to_datetime('2021-08-17'), color="green", linestyle="--",  label='Fase Verde')
+            # ax.axvline(pd.to_datetime('2022-01-15'), color="m", linestyle="--",  label='Quebra')
         
         # Verify if a secondary axis is necessary 
         if len(covid_variables) > 0:
@@ -157,7 +168,7 @@ class CorrelationWithCovid:
             
             ax2.set_ylim(0)
             ax2.set_ylabel(label_y2, color='k', fontsize=20, labelpad = 20)
-            ax2.legend(bbox_to_anchor = (1.15,  0.35), loc = 'lower left', fontsize = 18)
+            ax2.legend(bbox_to_anchor = (0,  1), loc = 'lower left', fontsize = 18)
             for label in (ax2.get_xticklabels() + ax2.get_yticklabels()):
                 label.set_fontsize(16)
         else:
@@ -168,13 +179,21 @@ class CorrelationWithCovid:
         ax.set_xlabel('Date', fontsize=20, labelpad = 20)
         ax.set_ylabel(variable, color='k', fontsize=20, labelpad = 20)
         if len(covid_variables) == 0:
-            ax.legend(bbox_to_anchor = (1, 0.35), loc = 'upper left', fontsize = 18)
+            lgd = ax.legend(bbox_to_anchor = (0, 1), loc = 'upper left', fontsize = 18)
         else:
-            ax.legend(bbox_to_anchor = (1.15, 0.35), loc = 'upper left', fontsize = 18)
+            lgd = ax.legend(bbox_to_anchor = (0, 1), loc = 'upper left', fontsize = 18)
         
         # Set tick font size
         for label in (ax.get_xticklabels() + ax.get_yticklabels()):
             label.set_fontsize(16)
+        
+        
+        # set monthly locator
+        ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+        # set formatter
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
+        # set font and rotation for date tick labels
+        plt.gcf().autofmt_xdate()
         
         plt.title(title, size=25, pad = 20)
 
@@ -182,7 +201,8 @@ class CorrelationWithCovid:
         plt.axis([None, None, 0, None])
         
         if save:
-            filename = variable + '_vs_'+ str(covid_variables) + '.png'
-            plt.savefig(self.destination_folder_path + 'versus_covid/' + filename)
+            filename = variable + '_vs_'+ str(covid_variables) + '_' + str(show_key_pandemic_moments) + '_' + str(show_all_phases) +  '.png'
+            plt.savefig(self.destination_folder_path + 'versus_covid/' + filename, bbox_extra_artists=(lgd,), bbox_inches='tight')
+            # fig.savefig('samplefigure', bbox_extra_artists=(lgd,), bbox_inches='tight')
         
         plt.show()
