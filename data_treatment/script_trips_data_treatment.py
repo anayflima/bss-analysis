bfe14f3dd58d02bf779217cc89b3424b6c41b27e
Ana Lima <ayf.lima@usp.br>
2023-07-25 10:24:24 -0300

    docs: Add instruction comment about the data treatment script

--- data_treatment/script_trips_data_treatment.py
+++ data_treatment/script_trips_data_treatment.py
@@ -17,10 +17,14 @@ destination_folder_path = data_folder + 'trips/treated_data/'
 '''
 To treat only files that follow a specific name pattern:
 '''
-# filename = 'trips_BikeSampa_2020-*'
-# dt = DataTreatment(source_folder_path, destination_folder_path, filename)
+# filename = 'trips_BikeSampa_2022-*'
+# dt = TripsDataTreatment(source_folder_path, destination_folder_path, filename)
 
+'''
+To treat all files in the folder:
+'''
 dt = TripsDataTreatment(source_folder_path, destination_folder_path)
+
 dt.treat_trips_data()
 print('DataTreatment completed')
 
